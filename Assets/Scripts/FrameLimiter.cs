3e679ede416e661764a6ebbd0de7052e2cad2797
Kirbysuperstar <garethchoake@gmail.com>
2020-08-03 23:16:03 +1000

    Added frame limiter, defaults to 60fps.

---
+++ Assets/Scripts/FrameLimiter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameLimiter : MonoBehaviour
+{
+    public int targetFramerate = 60;
+
+    void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = targetFramerate;
+    }
+
+}
