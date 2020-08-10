2d085c16baead89407aa6bff10cbf52307e0a4f9
Kirbysuperstar <garethchoake@gmail.com>
2020-08-10 10:29:19 +1000

    Spawning stuff!

--- Assets/Scripts/SpawnHandler.cs
+++ Assets/Scripts/SpawnHandler.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class SpawnHandler : MonoBehaviour
 {
     // The enemy prefabs
-    public GameObject enemy1;
-    public GameObject enemy2;
-    public GameObject enemy3;
+    //public GameObject enemy1;
+    //public GameObject enemy2;
+    //public GameObject enemy3;
+
+    public GameObject[] enemyPrefabs;
 
     // vector3 spawn positions for the enemies. Realistically only the y-axis changes, but having options is never bad.
     public Vector3 enemy1SpawnPos;
@@ -20,6 +22,9 @@ public class SpawnHandler : MonoBehaviour
     public bool timerRunning = false;
 
     public string spawnSpeed = "slow";
+
+    private float startDelay = 2;
+    private int spawnLine;
 
     // Start is called before the first frame update
     void Start()
@@ -47,26 +52,39 @@ public class SpawnHandler : MonoBehaviour
 
             if (countdownTimer < 30)
             {
-                Debug.Log("Second time threshold reached - increasding speed to fast.");
+                Debug.Log("Second time threshold reached - increasing speed to fast.");
                 spawnSpeed = "fast";
                 timerRunning = false;
             }
         }
         //spawn enemies at intervals
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnRandomEnemy();
+        }
     }
 
-    void SpawnEnemy1()
+    void SpawnRandomEnemy()
     {
-        Instantiate(enemy1, enemy1SpawnPos, enemy1.transform.rotation);
-    }
-
-    void SpawnEnemy2()
-    {
-        Instantiate(enemy2, enemy2SpawnPos, enemy2.transform.rotation);
-    }
-
-    void SpawnEnemy3()
-    {
-        Instantiate(enemy3, enemy3SpawnPos, enemy3.transform.rotation);
+        spawnLine = Random.Range(0, 3);
+        Debug.Log("Spawnline is " + spawnLine);
+        if (spawnLine == 0)
+        {
+            Vector3 spawnPos = enemy1SpawnPos;
+            int enemyIndex = 0;
+            Instantiate(enemyPrefabs[enemyIndex], spawnPos, enemyPrefabs[enemyIndex].transform.rotation);
+        }
+        if (spawnLine == 1)
+        {
+            Vector3 spawnPos = enemy2SpawnPos;
+            int enemyIndex = 1;
+            Instantiate(enemyPrefabs[enemyIndex], spawnPos, enemyPrefabs[enemyIndex].transform.rotation);
+        }
+        if (spawnLine == 2)
+        {
+            Vector3 spawnPos = enemy3SpawnPos;
+            int enemyIndex = 2;
+            Instantiate(enemyPrefabs[enemyIndex], spawnPos, enemyPrefabs[enemyIndex].transform.rotation);
+        }
     }
 }
