ce7ca3ca4f34ef651c0a36b64910b480f9a50d54
Kirbysuperstar <garethchoake@gmail.com>
2020-08-12 21:33:44 +1000

    Movement disabled when Game Over

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -27,7 +27,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MovePlayer();
+        if (isGameOver != true)
+        {
+            MovePlayer();
+        }
         if (Input.GetKeyDown(KeyCode.Space))
         {
             ShootShuriken();
